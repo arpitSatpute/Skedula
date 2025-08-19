5b3061fefb2b479f1dc73b509d0829caed8ae6fd
arpitSatpute <arpitrameshsatpute6986@gmail.com>
2025-08-19 16:23:08 +0530

    Cors

--- Backend/src/main/java/com/arpit/Skedula/Skedula/SkedulaApplication.java
+++ Backend/src/main/java/com/arpit/Skedula/Skedula/SkedulaApplication.java
@@ -3,8 +3,9 @@ package com.arpit.Skedula.Skedula;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-
+@CrossOrigin(origins = "*")
 @SpringBootApplication
 public class SkedulaApplication {
 	public static void main(String[] args) {
