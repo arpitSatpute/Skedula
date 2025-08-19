76afbe281c9b4e65366bc43c9fb7dc9f2f96b268
arpitSatpute <arpitrameshsatpute6986@gmail.com>
2025-08-19 15:06:16 +0530

    adding Docker

--- Backend/src/main/java/com/arpit/Skedula/Skedula/SkedulaApplication.java
+++ Backend/src/main/java/com/arpit/Skedula/Skedula/SkedulaApplication.java
@@ -1,25 +1,13 @@
 package com.arpit.Skedula.Skedula;
 
-import com.arpit.Skedula.Skedula.services.EmailService;
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
+
 
 @SpringBootApplication
-//@RequiredArgsConstructor
 public class SkedulaApplication {
-
-
-	//	private final EmailService emailService;
-
 	public static void main(String[] args) {
 		SpringApplication.run(SkedulaApplication.class, args);
 	}
-
-//	@EventListener(ApplicationReadyEvent.class)
-//	public void sendTestEmail() {
-//		emailService.sendEmail("arpitsatpute3964@gmail.com", "Email Service Testing", "This is a test email from Skedula application. If you received this email, the email service is working correctly.");
-//	}
 }
--- Backend/src/main/java/com/arpit/Skedula/Skedula/configs/RabbitConfig.java
+++ Backend/src/main/java/com/arpit/Skedula/Skedula/configs/RabbitConfig.java
@@ -1,30 +1,30 @@
-package com.arpit.Skedula.Skedula.configs;
-
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-
-
-@Configuration
-public class RabbitConfig {
-
-    @Bean
-    public Queue emailQueue() {
-        return new Queue("emailQueue", true);
-    }
-    @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
-        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setMessageConverter(jsonMessageConverter());
-        return rabbitTemplate;
-    }
-
-    @Bean
-    public Jackson2JsonMessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
-    }
-}
+//package com.arpit.Skedula.Skedula.configs;
+//
+//import org.springframework.amqp.core.Queue;
+//import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+//import org.springframework.amqp.rabbit.core.RabbitTemplate;
+//import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//
+//
+//@Configuration
+//public class RabbitConfig {
+//
+//    @Bean
+//    public Queue emailQueue() {
+//        return new Queue("emailQueue", true);
+//    }
+//    @Bean
+//    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+//        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+//        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+//        return rabbitTemplate;
+//    }
+//
+//    @Bean
+//    public Jackson2JsonMessageConverter jsonMessageConverter() {
+//        return new Jackson2JsonMessageConverter();
+//    }
+//}
