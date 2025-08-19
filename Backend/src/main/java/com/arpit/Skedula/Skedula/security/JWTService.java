64505cb81b8e43079855b2e1f39b03196d935e3d
arpitSatpute <arpitrameshsatpute6986@gmail.com>
2025-08-19 17:12:49 +0530

    token time

--- Backend/src/main/java/com/arpit/Skedula/Skedula/security/JWTService.java
+++ Backend/src/main/java/com/arpit/Skedula/Skedula/security/JWTService.java
@@ -30,7 +30,7 @@ public class JWTService {
                 .claim("email", user.getEmail())
                 .claim("roles", user.getRoles().toString())
                 .issuedAt(new Date())
-                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 1 ))
+                .expiration(new Date(System.currentTimeMillis() + 1000L * 60 * 60 * 24 * 180)) // 6 months
                 .signWith(getSecretKey())
                 .compact();
     }
